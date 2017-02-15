096afba900c7fbef277d414a09d3bb5fd2eec76c
Jennifer <greven.jen@web.de>
2017-02-15 15:02:32 +0100

    Abweichung beim Einparken

--- src/segfault_einparken.cpp
+++ src/segfault_einparken.cpp
@@ -77,7 +77,7 @@ int main(int argc, char **argv)
 	    ros::Subscriber sensor_sub = nctrl.subscribe<pses_basis::SensorData>("pses_basis/sensor_data", 10, std::bind(sensorCallback, std::placeholders::_1, &sensors));
         ros::Subscriber carInfo_sub = nctrl.subscribe<pses_basis::CarInfo>("pses_basis/car_info",10, std::bind(carInfoCallback, std::placeholders::_1, &carInfo));
 
-        ros::Time::init();
+
 
         float d1= 0;
         float d2= 0;
@@ -85,39 +85,33 @@ int main(int argc, char **argv)
         float avgSensR[3] = {0.0,0.0,0.0};
         float avgSensRight=1000;
         float distanceToWall= 6;
-        float epsilon = 0.01;
+        float epsilon = 0.1;
 
         bool firstLoop = false;
         bool luecke=false;
 
         float breiteLuecke;
 
-        double time1 = ros::Time::now().toSec();
-        double time2 = ros::Time::now().toSec();
-        double time3 = ros::Time::now().toSec();
-        double time4 = ros::Time::now().toSec();
-
-	    const float PI = 3.14159265;
-
-
         //---  Einparken  ---
-        //Breite vom Auto
-        float a=0.25;
-        //von Hinterachse bis vorne
-        float b=0.34;
-        //Radabstand
-        float radabstand=0.155;
-        //maximaler Lenkwinkel
-        float phi_lenk=24;
-        //Radius des Autos bei max Einlenkung
-        float R=(2*radabstand)/sin(phi_lenk*PI/180);
-        float h=sqrt(b*b-a*a+2*a*R);
-        //luecke ca 70 cm
-        float parkluecke =2*h-b;
-        //Rotationsradius
-        float R_0=(b*b)/(2*a);
-        //Winkel bis Wendepunkt erreicht in RAD
-        float theta=acos(1-(a/(R+R_0)));
+        const float PI = 3.14159265f;
+         //Breite vom Auto
+         float a=0.25f;
+         //von Hinterachse bis vorne
+         float b=0.34f;
+         //Radabstand
+         float radabstand=0.155f;
+         //maximaler Lenkwinkel
+         float phi_lenk=24;
+         //Radius des Autos bei max Einlenkung
+         float R=0.63; //gemessen; Formel:(2*radabstand)/sin(phi_lenk*PI/180);
+         float h=sqrt(b*b-a*a+2*a*R);
+         //luecke ca 70 cm
+         //TODO Off-Set bestimmen
+         float parkluecke =2*0.7;//2*h-b+0.3;//TODO:Parklückengröße? Formel:2*h-b+0.3;
+         //Rotationsradius
+         float R_0=(b*b)/(2*a);
+         //Winkel bis Wendepunkt erreicht in RAD
+         float theta=acos(1-(a/(R+R_0)));
          //Wendepunkt bereits erreicht?
          bool wendepunkt=false;
          bool haltepunkt=false;
@@ -127,6 +121,11 @@ int main(int argc, char **argv)
          //gesamtstrecke start und ende der parkluecke
          float startPL;
          float endPL = 0;
+
+	 //Yaws vor und nach der Lücke
+	 float startYaw;
+	 float endYaw;
+	 float diffYaw;
          //--------
 
          float factor;
@@ -159,12 +158,15 @@ int main(int argc, char **argv)
                                          if (avgSensRight > distanceToWall + epsilon) {
                                                  schleife=true;
                                                  startPL = carInfo.driven_distance;
+						 startYaw=currentYaw;
                                          }
                                  }else{
                                      if (schleife) {
                                              endPL = carInfo.driven_distance;
                                              if(endPL-startPL >= parkluecke ){
                                                      luecke = true;
+						     endYaw=currentYaw;
+						     diffYaw=endYaw-startYaw;
                                                      currentYaw=0;
                                              }else if(avgSensRight < distanceToWall - epsilon){
                                                      schleife = false;
@@ -175,9 +177,9 @@ int main(int argc, char **argv)
 
                          //Einparken
                          if(luecke){
-                                 cmd.motor_level=-5;
+                                 cmd.motor_level=-14;
 
-                                 if ((wendepunkt==false)&&(fabs(currentYaw)<fabs(theta))){
+                                 if ((wendepunkt==false)&&(fabs(currentYaw)<(fabs(theta)-diffYaw))){
                                          cmd.steering_level=-50;
                                  } else if ((haltepunkt==false)&&(wendepunkt==true)&&(fabs(currentYaw)>0)){
                                          cmd.steering_level=50;
@@ -191,20 +193,20 @@ int main(int argc, char **argv)
                                  if ((wendepunkt==true)&&(currentYaw<=0)){
                                    haltepunkt=true;
                                  }
-                        }
+                         }
 
-                }
+                 }
 
-                        // compute control error
-                        //    regc.control(sensors,cmd);
-                        //tt.con(cmd);
-                        //ROS_INFO( "PADDINGL; yaw: [%f]; yf_avg: [%f]; yr: [%f]; er: [%f]; DistFront: [%f]: ; Speed [%f]; steering: [%d]; yr_avg: [%f]", regc.yaw, regc.yf_avg,regc.yr, regc.er, sensors.range_sensor_front, carInfo.speed, cmd.steering_level, regc.yr_avg );
+                         // compute control error
+                         //   regc.control(sensors,cmd);
+                         //tt.con(cmd);
+                         ROS_INFO("PADDINGL; Dist:[%f] ; DistRight: [%f]", carInfo.driven_distance, sensors.range_sensor_right);
+                         //ROS_INFO( "PADDINGL; yaw: [%f]; yf_avg: [%f]; yr: [%f]; er: [%f]; DistFront: [%f]: ;  Speed [%f]; steering: [%d]; yr_avg: [%f]", regc.yaw, regc.yf_avg,regc.yr, regc.er, sensors.range_sensor_front, carInfo.speed, cmd.steering_level, regc.yr_avg );
 
-                        // publish
-                        commands_pub.publish(cmd);
-                        ros::spinOnce();
-                        loop_rate.sleep();
-        }
-        ros::spin();
-}
-
+                         // publish
+                         commands_pub.publish(cmd);
+                         ros::spinOnce();
+                         loop_rate.sleep();
+         }
+         ros::spin();
+ }
